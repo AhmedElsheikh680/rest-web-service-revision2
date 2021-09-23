b23afe261d0f045d994a1b423bcea3e123b1f247
ahmed.elsheikh <ahmed.elsheikh>
2021-09-23 10:14:55 +0200

    adding HATEOAS

--- src/main/java/com/rest/api/controller/UserController.java
+++ src/main/java/com/rest/api/controller/UserController.java
@@ -1,9 +1,10 @@
 package com.rest.api.controller;
 
-import com.rest.api.exception.UserNotFoundException;
 import com.rest.api.model.User;
 import com.rest.api.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -11,6 +12,9 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 public class UserController {
@@ -24,8 +28,17 @@ public class UserController {
     }
 
     @GetMapping("/users/{id}")
-    public User getUser(@PathVariable int id){
-       return userService.findUser(id);
+    public EntityModel<User> getUser(@PathVariable int id){
+       User user =  userService.findUser(id);
+    // "users", SERVER_PATH, "/users"
+    //retrieveAllUsers
+    //HATEOAS
+        EntityModel<User> resource = EntityModel.of(user);
+        WebMvcLinkBuilder linkTo = linkTo(methodOn(this.getClass()).users());
+        resource.add(linkTo.withRel("users"));
+
+       return resource;
+
 //        User user =  userService.findUser(id);
 //        if(user == null)
 //            throw new UserNotFoundException("id-> "+id);
--- src/main/java/com/rest/api/service/UserService.java
+++ src/main/java/com/rest/api/service/UserService.java
@@ -1,12 +1,10 @@
 package com.rest.api.service;
-
 import com.rest.api.exception.UserNotFoundException;
 import com.rest.api.model.User;
 import com.rest.api.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Date;
 import java.util.List;
 
 @Service
